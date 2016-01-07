8188bd8d8d06c75a4a100020c66933cace2c6b13
smlgroot <smlg@Sergios-MacBook-Pro.local>
2016-01-07 14:11:21 -0600

    first

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ var io = require('socket.io')(httpServer);
 io.on('connection', function(socket) {
   socket.emit('news', {
     hello: 'world'
-  });
+  }); 
   socket.on('my other event', function(data) {
     console.log(data);
   });
